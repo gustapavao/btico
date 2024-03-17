00ce4969f62c656f2e323e45ec211c329a7e13c3
Arthur Silva <tuzaum.silva@gmail.com>
2024-03-17 20:59:12 -0300

    adding commmands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime
 from discord.ext import commands
 from dotenv import dotenv_values
 
@@ -32,11 +33,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('ping'):
+    if message.content.startswith('!ping'):
         await message.channel.send('pong')
 
-    if message.content.startswith("github"):
+    if message.content.startswith("!github"):
         await message.channel.send('https://github.com/gustapavao/btico')
+
+    if message.content.startswith("!hora"):
+        hoje = datetime.now()
+        hoje_string = hoje.strftime("%H:%M")
+        await message.channel.send(f'Agora são {hoje_string} em ponto.')
 
 
 # commands
