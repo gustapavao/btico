58cc6f5b107516bfe0bc075f70a6ea396ffdceb7
pavao <luisgustavopavao@hotmail.com>
2024-03-23 00:38:10 -0300

    v1.0

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 from dotenv import dotenv_values
-
+import asyncio
+from commands.manager import Manager
 
 # dotenv variables
 config = dotenv_values(".env")
@@ -14,6 +15,8 @@ SERVER_ID = config["SERVER_ID"]
 intents = discord.Intents.default()
 intents.message_content = True
 
+# class
+manager = Manager()
 
 # discord connection
 client = discord.Client(intents=intents)
@@ -30,6 +33,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print(message)
     if message.author == client.user:
         return
 
@@ -44,10 +48,64 @@ async def on_message(message):
         hoje_string = hoje.strftime("%H:%M")
         await message.channel.send(f'Agora são {hoje_string} em ponto.')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    try:
+        channels = message.guild.channels
+        for channel in channels:
+            if channel.name == "🐦┇avisos":
+                channelreply = channel
+    except:
+        print("Canal privado ---", message)
+        channelreply = message.channel
 
-# commands
-@bot.command()
-async def github(ctx):
-    await ctx.send('https://github.com/gustapavao/btico')
+
+    if message.content.startswith("!noclasstomorrow"):
+        if manager.checkAutor(message):
+            reply = manager.noClassTomorrow(message)
+            await channelreply.send(reply)
+        else:
+            await message.channel.send("Você não pode realizar essa ação")
+    elif message.content.startswith("!aviso"):
+        if manager.checkAutor(message):
+            reply = manager.shareMessage(message)
+            await channelreply.send(reply)
+        else:
+            await message.channel.send("Você não pode realizar essa ação")
+    elif message.content.startswith("!newtask"):
+        if manager.checkAutor(message):
+            reply = manager.newTask(message)
+            await channelreply.send(reply)
+        else:
+            await message.channel.send("Você não pode realizar essa ação")
+    elif message.content.startswith("!github"):
+        reply = manager.github
+        await message.channel.send(reply)
+    elif message.content.startswith("!monitoria"):
+        reply = manager.monitoria(message)
+        await message.channel.send(reply)
+    elif message.content.startswith("!anderson"):
+        reply = manager.anderson()
+        await message.channel.send(reply)
+    elif message.content.startswith("!help"):
+        reply = manager.help()
+        await message.channel.send(reply)
+    elif message.content.startswith("!f5"):
+        if manager.checkAutor(message):
+            reply = manager.atualizacao()
+            await channelreply.send(reply)
+        else:
+            await message.channel.send("Você não pode realizar essa ação")
+    elif message.content.startswith("!provas"):
+        reply = manager.nextTests()
+        await message.channel.send(reply)
+    elif message.content.startswith("!avprovas"):
+        if manager.checkAutor(message):
+            reply = manager.nextTests()
+            await channelreply.send(reply)
+        else:
+            await message.channel.send("Você não pode realizar essa ação")
 
 client.run(TOKEN)
